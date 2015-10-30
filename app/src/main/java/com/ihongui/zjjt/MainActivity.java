0b60ff08fda189177d0196e63b73909260a205bd
PaddingtonLee <lihy_1989@sina.cn>
2015-10-30 20:47:32 +0800

    在IndexActivity中添加了跳转的样式， 添加了res/anim 修正了styles.xml，即使用了Android5.0系统默认的蓝色title

--- app/src/main/java/com/ihongui/zjjt/MainActivity.java
+++ app/src/main/java/com/ihongui/zjjt/MainActivity.java
@@ -1,11 +1,16 @@
 package com.ihongui.zjjt;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //用来记录时间
+    private long mExitTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +38,27 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * 按两次back键才退出
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if ((System.currentTimeMillis() - mExitTime) > 1500) {
+                Object mHelperUtils;
+                Toast.makeText(this, "再按一次退出程序", 800).show();
+                mExitTime = System.currentTimeMillis();
+            } else {
+                finish();
+                overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
